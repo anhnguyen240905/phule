5d1ad7fc5e08568983be99e4498d5a9a46e93c84
anhnguyen240905 <k62.2315510021@ftu.edu.vn>
2025-11-03 11:57:44 +0700

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const foods = [
+  "Bánh tráng cuốn thịt heo", "Bánh bèo", "Bánh căn", "Bánh canh ruộng",
+  "Bánh đập", "Bánh mì bột lọc", "Bánh tráng kẹp", "Bê thui",
+  "Bánh xèo", "Bún bò", "Bún chả cá", "Bún mắm nêm",
+  "Cá nục hấp bánh tráng", "Cao lầu", "Chả bò", "Cơm hến",
+  "Cơm gà", "Gỏi cá Nam Ô", "Chè sầu riêng"
+];
