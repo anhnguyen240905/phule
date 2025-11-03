815c3b928a3a54cc3dab96d4f935a62c9733eac2
anhnguyen240905 <k62.2315510021@ftu.edu.vn>
2025-11-03 13:23:07 +0700

    Update SelectFoods.jsx

--- src/pages/SelectFoods.jsx
+++ src/pages/SelectFoods.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 export default function SelectFood({ next, saveSelection }) {
   // Danh sách món ăn (bạn có thể chỉnh lại tùy ý)
   const foods = [
