ad4fa4b25bea3a76e89a30cd70909e31b4021476
anhnguyen240905 <k62.2315510021@ftu.edu.vn>
2025-11-04 08:31:30 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,12 @@ export default function App(){
       <div className="w-full max-w-4xl">
         {step === 0 && <Welcome next={() => setStep(1)} />}
         {step === 1 && (
-          <SelectFoods
-            selected={selected}
-            setSelected={setSelected}
-            saveSelection={saveSelection}   // ✅ TRUYỀN PROP NÀY XUỐNG
-            next={() => setStep(2)}
-          />
-        )}
+  <SelectFoods
+    selected={selected}
+    setSelected={setSelected}
+    next={() => setStep(2)} // PHẢI là hàm, không phải setStep(2) trực tiếp
+  />
+)}
         {step === 2 && <ChooseFavorite selected={selected} setFavorite={setFavorite} setNextTry={setNextTry} next={() => setStep(3)} back={() => setStep(1)} />}
         {step === 3 && <Review setReview={setReview} next={() => setStep(4)} back={() => setStep(2)} />}
         {step === 4 && <Result selected={selected} favorite={favorite} nextTry={nextTry} review={review} restart={() => {
